cad84a116172781c2e2c8b064f0f9e20af2cdc94
sunnytangdou <2812493266@qq.com>
2019-11-18 00:27:46 +0800

    Tips: Modify income page

--- src/app/income/income.component.ts
+++ src/app/income/income.component.ts
@@ -6,16 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income.component.css']
 })
 export class IncomeComponent implements OnInit {
-  public listOfOption = ['2019年08月', '2019年09月', '2019年10月', '2019年11月', '2019年12月'];
-  public selectedValue = this.listOfOption.length ? this.listOfOption[0] : '1970年01月';
+  public dateList = ['2019年08月', '2019年09月', '2019年10月', '2019年11月', '2019年12月'];
+  public selectedDate = this.dateList.length ? this.dateList[0] : '1970年01月';
+  public accountList = ['全部账户', '账户A', '账户B'];
+  public selectedAccount = this.accountList.length ? this.accountList[0] : '全部账户';
   constructor() {
   }
 
   ngOnInit() {
   }
 
-  timeChange(time) {
-    console.log(time);
+  dateChange(date) {
+    console.log(date);
+    // TODO Set selectValue to redux.
+  }
+
+  accountChange(account) {
+    console.log(account);
     // TODO Set selectValue to redux.
   }
 
